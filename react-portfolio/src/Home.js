56793dbf7d57e9ab52174d495f99ddc5427ac2fe
kanokpich18 <s6603052421186@email.kmutnb.ac.th>
2025-03-24 19:22:03 +0700

    update

--- react-portfolio/src/Home.js
+++ react-portfolio/src/Home.js
@@ -1,19 +1,18 @@
 import React from "react";
 import './Home.css';
 
+
+
 const Home = () => {
   return (
     <div className="home-container">
       <div className="home-content">
         <h1 className="title">Welcome to My Portfolio</h1>
-        <p className="description">
-          I am a passionate developer with a focus on building clean and
-          modern web applications.
-        </p>
-        <a href="#projects" className="btn">Explore Projects</a>
+  
       </div>
     </div>
   );
 };
 
 export default Home;
+
